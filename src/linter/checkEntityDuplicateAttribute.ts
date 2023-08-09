474634e0732f264c28721df645ebf12b3a6e5654
Matías López <matiaslg965@gmail.com>
2023-08-09 18:10:15 -0400

    fix(linter): detects duplicate attributes in child entity

--- src/linter/checkEntityDuplicateAttribute.ts
+++ src/linter/checkEntityDuplicateAttribute.ts
@@ -9,7 +9,9 @@ export const checkEntityDuplicateAttribute = (
 
   for (const entity of er.entities) {
     const attributeNameFrequency = new Map<string, number>();
-    for (const attribute of entity.attributes) {
+    const entityAttributes = getAllAttributes(entity.name, er);
+
+    for (const attribute of entityAttributes) {
       const name = attribute.name;
       if (attributeNameFrequency.has(name))
         attributeNameFrequency.set(name, attributeNameFrequency.get(name)! + 1);
@@ -18,8 +20,8 @@ export const checkEntityDuplicateAttribute = (
 
     for (const [attrName, freq] of attributeNameFrequency) {
       if (freq > 1) {
-        const lastAttribute = entity.attributes
-          .filter((e) => e.name === attrName)
+        const lastAttribute = entityAttributes
+          .filter((e) => e.name === attrName && !e.isFromParent)
           .pop()!;
         errors.push({
           type: SemanticErrorType.ENTITY_DUPLICATE_ATTRIBUTE,
@@ -32,4 +34,29 @@ export const checkEntityDuplicateAttribute = (
   }
 
   return errors;
+};
+
+const getAllAttributes = (entityName: string, er: ER) => {
+  let entity = er.entities.filter((e) => e.name === entityName).pop()!;
+  const entityAttributes = entity.attributes.map((attr) => ({
+    isFromParent: false,
+    ...attr,
+  }));
+
+  if (!entity.hasParent) return entityAttributes;
+  let hasParent = true;
+  while (hasParent) {
+    // assume only one parent, more than 1 would be an error caught by another validator
+    const parent = er.entities.filter((e) => e.name === entity.parentName);
+    if (parent.length === 0) hasParent = false;
+    parent[0].attributes.forEach((attr) => {
+      entityAttributes.push({
+        isFromParent: true,
+        ...attr,
+      });
+    });
+    entity = parent[0];
+    hasParent = entity.hasParent;
+  }
+  return entityAttributes;
 };
--- test/linter/entity_duplicate_attribute.test.ts
+++ test/linter/entity_duplicate_attribute.test.ts
@@ -41,6 +41,21 @@ describe("Linter detects duplicate attributes in entities", () => {
     expect(errors.find((error) => error.entityName === "Song")).toBeDefined();
     expect(errors.find((error) => error.entityName === "Dog")).toBeDefined();
   });
+
+  it("detects error when duplicate is in subclass", () => {
+    const errors = checkEntityDuplicateAttribute(ERWithWrongSubclass);
+    expect(errors).toHaveLength(1);
+    expect(errors[0].type).toBe(SemanticErrorType.ENTITY_DUPLICATE_ATTRIBUTE);
+    expect(errors[0].entityName).toBe("Car");
+    expect(errors[0].attributeName).toBe("Brand");
+  })
+
+  it("detects error when duplicate is in subsubclass", () => {
+    const errors = checkEntityDuplicateAttribute(ERWithWrongSubclass2);
+    expect (errors).toHaveLength(1);
+    expect(errors[0].entityName).toBe("Ferrari");
+    expect(errors[0].attributeName).toBe("Brand");
+  })
 });
 
 /*
@@ -464,3 +479,455 @@ const ERWith2WrongEntities: ER = {
   relationships: [],
   aggregations: [],
 };
+
+
+/*
+entity Vehicle {
+  id key
+  Brand
+  License_plate_number}
+
+entity Car extends Vehicle {
+    Max_speed
+    Brand
+    N_passengers
+}
+
+entity Truck extends Vehicle {
+  Tonnage
+  N_of_axles
+}
+*/
+const ERWithWrongSubclass: ER = 
+{
+  entities: [
+    {
+      type: 'entity',
+      name: 'Vehicle',
+      attributes: [
+        {
+          name: 'id',
+          location: {
+            start: {
+              offset: 19,
+              line: 2,
+              column: 3
+            },
+            end: {
+              offset: 25,
+              line: 2,
+              column: 9
+            }
+          },
+          isKey: true,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'Brand',
+          location: {
+            start: {
+              offset: 28,
+              line: 3,
+              column: 3
+            },
+            end: {
+              offset: 33,
+              line: 3,
+              column: 8
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'License_plate_number',
+          location: {
+            start: {
+              offset: 36,
+              line: 4,
+              column: 3
+            },
+            end: {
+              offset: 56,
+              line: 4,
+              column: 23
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        }
+      ],
+      hasParent: false,
+      parentName: null,
+      hasDependencies: false,
+      dependsOn: null,
+      location: {
+        start: {
+          offset: 0,
+          line: 1,
+          column: 1
+        },
+        end: {
+          offset: 57,
+          line: 4,
+          column: 24
+        }
+      }
+    },
+    {
+      type: 'entity',
+      name: 'Car',
+      attributes: [
+        {
+          name: 'Max_speed',
+          location: {
+            start: {
+              offset: 92,
+              line: 7,
+              column: 5
+            },
+            end: {
+              offset: 101,
+              line: 7,
+              column: 14
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'Brand',
+          location: {
+            start: {
+              offset: 106,
+              line: 8,
+              column: 5
+            },
+            end: {
+              offset: 111,
+              line: 8,
+              column: 10
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'N_passengers',
+          location: {
+            start: {
+              offset: 116,
+              line: 9,
+              column: 5
+            },
+            end: {
+              offset: 128,
+              line: 9,
+              column: 17
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        }
+      ],
+      hasParent: true,
+      parentName: 'Vehicle',
+      hasDependencies: false,
+      dependsOn: null,
+      location: {
+        start: {
+          offset: 59,
+          line: 6,
+          column: 1
+        },
+        end: {
+          offset: 130,
+          line: 10,
+          column: 2
+        }
+      }
+    },
+    {
+      type: 'entity',
+      name: 'Truck',
+      attributes: [
+        {
+          name: 'Tonnage',
+          location: {
+            start: {
+              offset: 165,
+              line: 13,
+              column: 3
+            },
+            end: {
+              offset: 172,
+              line: 13,
+              column: 10
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'N_of_axles',
+          location: {
+            start: {
+              offset: 175,
+              line: 14,
+              column: 3
+            },
+            end: {
+              offset: 185,
+              line: 14,
+              column: 13
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        }
+      ],
+      hasParent: true,
+      parentName: 'Vehicle',
+      hasDependencies: false,
+      dependsOn: null,
+      location: {
+        start: {
+          offset: 132,
+          line: 12,
+          column: 1
+        },
+        end: {
+          offset: 187,
+          line: 15,
+          column: 2
+        }
+      }
+    }
+  ],
+  relationships: [],
+  aggregations: []
+}
+
+/*
+entity Vehicle {
+  id key
+  Brand
+  License_plate_number}
+
+entity Car extends Vehicle {
+    Max_speed
+    N_passengers
+}
+
+entity Ferrari extends Car {
+  Brand
+  Color
+}
+*/
+const ERWithWrongSubclass2: ER =
+{
+  entities: [
+    {
+      type: 'entity',
+      name: 'Vehicle',
+      attributes: [
+        {
+          name: 'id',
+          location: {
+            start: {
+              offset: 19,
+              line: 2,
+              column: 3
+            },
+            end: {
+              offset: 25,
+              line: 2,
+              column: 9
+            }
+          },
+          isKey: true,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'Brand',
+          location: {
+            start: {
+              offset: 28,
+              line: 3,
+              column: 3
+            },
+            end: {
+              offset: 33,
+              line: 3,
+              column: 8
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'License_plate_number',
+          location: {
+            start: {
+              offset: 36,
+              line: 4,
+              column: 3
+            },
+            end: {
+              offset: 56,
+              line: 4,
+              column: 23
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        }
+      ],
+      hasParent: false,
+      parentName: null,
+      hasDependencies: false,
+      dependsOn: null,
+      location: {
+        start: {
+          offset: 0,
+          line: 1,
+          column: 1
+        },
+        end: {
+          offset: 57,
+          line: 4,
+          column: 24
+        }
+      }
+    },
+    {
+      type: 'entity',
+      name: 'Car',
+      attributes: [
+        {
+          name: 'Max_speed',
+          location: {
+            start: {
+              offset: 92,
+              line: 7,
+              column: 5
+            },
+            end: {
+              offset: 101,
+              line: 7,
+              column: 14
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'N_passengers',
+          location: {
+            start: {
+              offset: 106,
+              line: 8,
+              column: 5
+            },
+            end: {
+              offset: 118,
+              line: 8,
+              column: 17
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        }
+      ],
+      hasParent: true,
+      parentName: 'Vehicle',
+      hasDependencies: false,
+      dependsOn: null,
+      location: {
+        start: {
+          offset: 59,
+          line: 6,
+          column: 1
+        },
+        end: {
+          offset: 120,
+          line: 9,
+          column: 2
+        }
+      }
+    },
+    {
+      type: 'entity',
+      name: 'Ferrari',
+      attributes: [
+        {
+          name: 'Brand',
+          location: {
+            start: {
+              offset: 153,
+              line: 12,
+              column: 3
+            },
+            end: {
+              offset: 158,
+              line: 12,
+              column: 8
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        },
+        {
+          name: 'Color',
+          location: {
+            start: {
+              offset: 161,
+              line: 13,
+              column: 3
+            },
+            end: {
+              offset: 166,
+              line: 13,
+              column: 8
+            }
+          },
+          isKey: false,
+          isComposite: false,
+          childAttributesNames: null
+        }
+      ],
+      hasParent: true,
+      parentName: 'Car',
+      hasDependencies: false,
+      dependsOn: null,
+      location: {
+        start: {
+          offset: 122,
+          line: 11,
+          column: 1
+        },
+        end: {
+          offset: 168,
+          line: 14,
+          column: 2
+        }
+      }
+    }
+  ],
+  relationships: [],
+  aggregations: []
+}
