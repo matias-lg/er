5242329b908655ddae7d62d1ed7f62a427349606
Matías López <matiaslg965@gmail.com>
2023-09-03 01:12:10 -0300

    style(app): fix header layout

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,9 +1,6 @@
 const Header = () => {
-  // lets create a Header for our app
   return (
-    <div className="bg-[#21252b] text-white flex flex-wrap justify-between w-full  p-4 border-b-2 border-b-slate-50/[0.16] shadow-2xl ">
       <div className="h-full w-1/3"> ER Diagram Editor </div>
-    </div>
   );
 };
 
