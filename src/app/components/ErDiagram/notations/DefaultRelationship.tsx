ff1f046068bbf0b8c55d52d092a6e6466bc2d368
Matías López <matiaslg965@gmail.com>
2023-09-15 13:20:37 -0300

    refactor(ErDiagram): duplicate code for handles refactor

--- src/app/components/ErDiagram/notations/DefaultRelationship.tsx
+++ src/app/components/ErDiagram/notations/DefaultRelationship.tsx
@@ -1,9 +1,25 @@
-import { memo } from "react";
+import { Fragment, memo } from "react";
 import { Handle, Position } from "reactflow";
 
 type DefaultRelationshipProps = {
-  data: { label: string; hasDependant: boolean };
+  data: {label: string; hasDependant: boolean}; 
 };
+
+const handleTypes = ["source", "target"] as const;
+
+const RelationshipHandles = ({ data }: DefaultRelationshipProps ) => 
+        // prettier-ignore
+        <>
+        {handleTypes.map((type) => (
+        <Fragment key={type}>
+            <Handle type={type} style={{ opacity: 0, transform: data.hasDependant ? "translate(-47px)" : "translate(-49px)",}} position={Position.Top} id="t"/>
+            <Handle type={type} style={{ opacity: 0, transform: data.hasDependant ? "translate(2px,-49px)" : "translate(0,-49px)",}} position={Position.Right} id="r" />
+            <Handle type={type} style={{ opacity: 0, transform: data.hasDependant ? "translate(-2px,42px)" : "translate(0px,42px)",}} position={Position.Left} id="l" />
+            <Handle type={type} style={{ opacity: 0, transform: data.hasDependant ? "translate(42px, 2px)" : "translate(43px)",}}     position={Position.Bottom} id="b"/>
+        </Fragment>
+        ))}
+        </>;
+
 
 const DefaultRelationship = ({ data }: DefaultRelationshipProps) => {
   {
@@ -15,102 +31,7 @@ const DefaultRelationship = ({ data }: DefaultRelationshipProps) => {
        } border-orange-700 bg-orange-200`}
       >
         <div className="-rotate-45">{data.label}</div>
-
-        <Handle
-          type="source"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(-47px)"
-              : "translate(-49px)",
-          }}
-          position={Position.Top}
-          id="t"
-        />
-
-        <Handle
-          type="target"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(-47px)"
-              : "translate(-49px)",
-          }}
-          position={Position.Top}
-          id="t"
-        />
-
-        <Handle
-          type="source"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(2px,-49px)"
-              : "translate(0,-49px)",
-          }}
-          position={Position.Right}
-          id="r"
-        />
-
-        <Handle
-          type="target"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(2px,-49px)"
-              : "translate(0,-49px)",
-          }}
-          position={Position.Right}
-          id="r"
-        />
-
-        <Handle
-          type="source"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(-2px,42px)"
-              : "translate(0px,42px)",
-          }}
-          position={Position.Left}
-          id="l"
-        />
-
-        <Handle
-          type="target"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(-2px,42px)"
-              : "translate(0px,42px)",
-          }}
-          position={Position.Left}
-          id="l"
-        />
-
-        <Handle
-          type="source"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(42px, 2px)"
-              : "translate(43px)",
-          }}
-          position={Position.Bottom}
-          id="b"
-        />
-
-        <Handle
-          type="target"
-          style={{
-            opacity: 0,
-            transform: data.hasDependant
-              ? "translate(42px, 2px)"
-              : "translate(43px)",
-          }}
-          position={Position.Bottom}
-          id="b"
-        />
+        <RelationshipHandles data={data} />
       </div>
     );
   }
