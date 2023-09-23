865b94b21fc19514b9b0a93a35980bca1295fe63
Matías López <matiaslg965@gmail.com>
2023-09-23 20:36:42 -0300

    better force layout

--- src/app/components/ErDiagram/useD3LayoutedElements.tsx
+++ src/app/components/ErDiagram/useD3LayoutedElements.tsx
@@ -9,13 +9,6 @@ import {
 } from "d3-force";
 import { adjustChildNodePosition } from "../../util/common";
 
-const simulation = forceSimulation()
-  .force("charge", forceManyBody().strength(-500))
-  .force("x", forceX().x(0).strength(0.05))
-  .force("y", forceY().y(0).strength(0.05))
-  .alphaTarget(0.05)
-  .stop();
-
 export const useD3LayoutedElements = () => {
   const { getNodes, setNodes, getEdges, fitView } = useReactFlow();
 
@@ -28,24 +21,30 @@ export const useD3LayoutedElements = () => {
   const D3LayoutElements = useCallback((): void => {
     const nodes = getNodes().map((node) => ({
       ...node,
-      x: node.position.x,
-      y: node.position.y,
+      x: 0,
+      y: 0,
     }));
     const edges = getEdges();
 
-    // If React Flow hasn't initialised our nodes with a width and height yet, or
-    // if there are no nodes in the flow, then we can't run the simulation!
+    // ensure reactflow has initialised nodes
     if (!initialised || nodes.length === 0) return;
 
-    simulation.nodes(nodes).force(
-      "link",
-      forceLink(edges)
-        .id((d) => (d as Edge).id)
-        .strength(0.05)
-        .distance(100),
-    );
+    forceSimulation()
+      .stop()
+      .force("charge", forceManyBody().strength(-500))
+      .force("x", forceX().x(0).strength(0.05))
+      .force("y", forceY().y(0).strength(0.05))
+      .alphaTarget(0.05)
+      .nodes(nodes)
+      .force(
+        "link",
+        forceLink(edges)
+          .id((d) => (d as Edge).id)
+          .strength(0.05)
+          .distance(100),
+      )
+      .tick(5500);
 
-    simulation.tick(1000);
     setNodes(nodes.map((node) => adjustChildNodePosition(node, nodes)));
     window.requestAnimationFrame(() => fitView());
   }, [initialised]);
