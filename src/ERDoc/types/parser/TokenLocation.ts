0f0e7447be886cbb8fb0f714529d336fd27417b8
Matías López <matiaslg965@gmail.com>
2023-09-25 20:07:37 -0300

    chore: don't use zod

--- src/ERDoc/types/parser/TokenLocation.ts
+++ src/ERDoc/types/parser/TokenLocation.ts
@@ -1,16 +1,12 @@
-import { z } from "zod";
-
-export const TokenLocationSchema = z.object({
-  start: z.object({
-    offset: z.number(),
-    line: z.number(),
-    column: z.number(),
-  }),
-  end: z.object({
-    offset: z.number(),
-    line: z.number(),
-    column: z.number(),
-  }),
-});
-
-export type TokenLocation = z.infer<typeof TokenLocationSchema>;
+export type TokenLocation = {
+  start: {
+    offset: number;
+    line: number;
+    column: number;
+  };
+  end: {
+    offset: number;
+    line: number;
+    column: number;
+  };
+};
