a568a5a1072487454ff5129479207eaab908c547
Matías López <matiaslg965@gmail.com>
2023-10-21 22:29:06 -0300

    fix(ControlPanel): use new API

--- src/app/components/ErDiagram/ControlPanel.tsx
+++ src/app/components/ErDiagram/ControlPanel.tsx
@@ -1,21 +1,46 @@
 import { useTranslations } from "next-intl";
 import { HiSparkles } from "react-icons/hi2";
-import { ControlButton, Controls } from "reactflow";
+import { ControlButton, Controls, useReactFlow } from "reactflow";
 import { colors } from "../../util/colors";
-import { useLayoutedElements } from "./hooks/useLayoutedElements";
+import { getLayoutedElements } from "./hooks/useLayoutedElements";
 
 type ControlPanelProps = {
   onLayoutClick: () => void;
 };
 
 export const ControlPanel = ({ onLayoutClick }: ControlPanelProps) => {
-  // const { layoutElements } = useLayoutedElements();
   const t = useTranslations("home.erDiagram");
+  const { getNodes, setNodes, getEdges, setEdges, fitView } = useReactFlow();
 
   const handleLayoutClick = () => {
-    // void layoutElements().then(() => {
-    //   setTimeout(onLayoutClick, 100);
-    // });
+    const nodes = getNodes();
+    const edges = getEdges();
+    getLayoutedElements(nodes, edges).then((layoutedNodes) => {
+      setNodes((nodes) =>
+        nodes.map((node) => {
+          const layoutedNode = layoutedNodes.find((n) => n.id === node.id);
+          return {
+            ...node,
+            position: layoutedNode?.position!,
+            style: { ...node.style, opacity: 1 },
+          };
+        }),
+      );
+
+      setEdges((edges) =>
+        edges.map((edge) => ({
+          ...edge,
+          hidden: false,
+          style: {
+            ...edge.style,
+          },
+        })),
+      );
+      setTimeout(() => {
+        window.requestAnimationFrame(() => fitView());
+        onLayoutClick();
+      }, 0);
+    });
   };
 
   return (
