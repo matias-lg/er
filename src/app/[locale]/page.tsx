9003fe62d68782abe62897fd5eed44cfbccf07ab
Matías López <matiaslg965@gmail.com>
2023-10-22 23:24:12 -0300

    style: run prettier

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,42 +6,41 @@ import { Context } from "../context";
 import { useContext, useState } from "react";
 
 const Page = () => {
-
   const [autoLayoutEnabled, setAutoLayoutEnabled] = useState<boolean>(false);
 
   return (
-    <Context.Provider value={{ autoLayoutEnabled, setAutoLayoutEnabled}}>
-    <Grid
-      templateAreas={`
+    <Context.Provider value={{ autoLayoutEnabled, setAutoLayoutEnabled }}>
+      <Grid
+        templateAreas={`
      "header"
      "main"
     `}
-      gridTemplateRows={"5% auto"}
-      h="100%"
-      gap="0"
-    >
-      <GridItem
-        mb={0}
-        display={"flex"}
-        backgroundColor={"#232730"}
-        borderBottom={"1px"}
-        borderBottomColor={"rgb(248 250 252 / 0.16)"}
-        area="header"
+        gridTemplateRows={"5% auto"}
+        h="100%"
+        gap="0"
       >
-        <Header />
-      </GridItem>
+        <GridItem
+          mb={0}
+          display={"flex"}
+          backgroundColor={"#232730"}
+          borderBottom={"1px"}
+          borderBottomColor={"rgb(248 250 252 / 0.16)"}
+          area="header"
+        >
+          <Header />
+        </GridItem>
 
-      <GridItem
-        overflow={"hidden"}
-        area={"main"}
-        display={"flex"}
-        w={"100%"}
-        flexDir={"row"}
-        justifyContent={"space-between"}
-      >
-        <Body />
-      </GridItem>
-    </Grid>
+        <GridItem
+          overflow={"hidden"}
+          area={"main"}
+          display={"flex"}
+          w={"100%"}
+          flexDir={"row"}
+          justifyContent={"space-between"}
+        >
+          <Body />
+        </GridItem>
+      </Grid>
     </Context.Provider>
   );
 };
