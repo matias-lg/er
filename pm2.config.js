fa3f0d5dd07e6a74af33b848cec321bfc5193bd3
Matías López <matiaslg965@gmail.com>
2023-11-11 21:01:03 -0300

    ci: deploy from remote

--- pm2.config.js
+++ pm2.config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   apps: [
     {
@@ -11,4 +13,17 @@ module.exports = {
       },
     },
   ],
+
+  deploy: {
+    production: {
+      user: process.env.DEPLOY_USER,
+      host: [{ host: process.env.DEPLOY_HOST, port: process.env.DEPLOY_PORT }],
+      key: process.env.DEPLOY_KEY,
+      ref: "origin/main",
+      repo: "https://github.com/matias-lg/er.git",
+      path: process.env.DEPLOY_PATH,
+      "post-deploy":
+        "npm i && npm run build:next && pm2 startOrRestart pm2.config.js",
+    },
+  },
 };
