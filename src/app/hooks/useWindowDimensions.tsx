6de7dd14fc9d67dc4b381f385577fd8a85b2bf99
Matías López <matiaslg965@gmail.com>
2023-11-14 21:29:46 -0300

    fix(hydration): resize listener on mobile

--- src/app/hooks/useWindowDimensions.tsx
+++ src/app/hooks/useWindowDimensions.tsx
@@ -1,27 +1,29 @@
 import { useState, useEffect } from "react";
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
+type WindowDimensions = {
+  width: number | undefined;
+  height: number | undefined;
+};
 
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState<{
-    width: number;
-    height: number;
-  }>(getWindowDimensions());
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimensions>({
+    width: undefined,
+    height: undefined,
+  });
 
   useEffect(() => {
     const handleResize = () => {
-      setWindowDimensions(getWindowDimensions());
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
-
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return windowDimensions;
-}
+};
+
+export default useWindowDimensions;
