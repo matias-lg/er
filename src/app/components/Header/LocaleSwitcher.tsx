eb1f3fef4174127c00f459188f01e7aee2741978
Matías López <matiaslg965@gmail.com>
2023-11-14 22:17:08 -0300

    fix(LocaleSwitcher): keep style same as other components

--- src/app/components/Header/LocaleSwitcher.tsx
+++ src/app/components/Header/LocaleSwitcher.tsx
@@ -1,36 +1,34 @@
-import { useLocale } from "next-intl";
+import { useTranslations } from "next-intl";
 import { usePathname, useRouter } from "next-intl/client";
-import { ChangeEvent, useTransition } from "react";
+import { useTransition } from "react";
+import { MdLanguage } from "react-icons/md";
+import { Dropdown } from "./Dropdown";
 
 const LocaleSwitcher = () => {
   const [isPending, startTransition] = useTransition();
-  const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
+  const t = useTranslations("home.header.localeSwitcher");
 
-  const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const nextLocale = e.target.value;
+  const onLocaleChange = (nextLocale: string) => {
+    if (isPending) return;
     startTransition(() => {
       router.push(pathname, { locale: nextLocale });
     });
   };
 
   return (
-    <div className="flex">
-      <select
-        className="inline-flex appearance-none bg-transparent py-3 pl-2 pr-6"
-        defaultValue={locale}
-        disabled={isPending}
-        onChange={onSelectChange}
-      >
-        {["en", "es"].map((locale) => (
-          <option key={locale} value={locale}>
-            {locale}
-          </option>
-        ))}
-      </select>
-      <span className="pointer-events-none text-center">⌄</span>
-    </div>
+    <Dropdown
+      title={
+        <div className="flex items-center">
+          <MdLanguage size={25} /> <span className="pl-2">{t("label")}</span>
+        </div>
+      }
+      items={["en", "es"].map((locale) => [
+        t(locale),
+        () => onLocaleChange(locale),
+      ])}
+    />
   );
 };
 
