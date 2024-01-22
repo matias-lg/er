959deda007891ea625c8e80deaec85e93cbff828
Matías López <matiaslg965@gmail.com>
2024-01-22 15:57:49 -0300

    fix(parser): detecting cycles in entity inheritance doesn't hang
    fix(parser): detecting cycles in entity inheritance doesn't hang

--- test/ERDoc/linter/entity_extends_child_entity.test.ts
+++ test/ERDoc/linter/entity_extends_child_entity.test.ts
@@ -11,18 +11,22 @@ describe("Linter detects when a child entity extends a child entity", () => {
     expect(errors[0].childEntityName).toBe("Dog");
   });
 
-  it("should return an error when an entity extends a child from multiple levels of inheritance", () => {
+  it("should return 1 error when an entity extends a child from multiple levels of inheritance", () => {
     const errors = checkEntityExtendsChildEntity(wrongERmultipleInheritance);
     expect(errors.length).toBe(1);
     expect(errors[0].type).toBe("ENTITY_EXTENDS_CHILD_ENTITY");
-    expect(errors[0].parentEntityName).toBe("Life");
-    expect(errors[0].childEntityName).toBe("Dog");
   });
 
   it("should return an empty array when no entity extends a child entity", () => {
     const errors = checkEntityExtendsChildEntity(correctER);
     expect(errors.length).toBe(0);
   });
+
+  it("shouldn't hang when there is a cyclic dependency", () => {
+    const errors = checkEntityExtendsChildEntity(cyclicER);
+    expect(errors.length).toBe(1);
+  });
+
 });
 
 const wrongER: ER = parse(`entity Dog extends Animal {
@@ -49,6 +53,29 @@ entity Animal extends Life {
 entity Life extends Dog {
 	l_id key
 }`);
+
+const cyclicER: ER = parse(`
+
+entity Life extends Westie {
+	age
+}
+
+entity Mammal extends Life {
+	m_id
+}
+
+entity Dog extends Mammal {
+	name
+}
+
+entity Westie extends Dog {
+
+}
+
+entity Frenchie extends Dog {
+
+}`
+);
 
 const correctER: ER = parse(`entity Dog extends Mammal {
     breed
